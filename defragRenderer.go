103eb24785eefcbbcc0cd26c906170b964e0a4d6
Allen Petersen <playa1@gmail.com>
2018-01-07 17:20:50 -0800

    Add fade to render

--- defragRenderer.go
+++ defragRenderer.go
@@ -8,25 +8,45 @@ import (
 	"os"
 )
 
+var palette = []color.Color{
+	color.RGBA{0x2d, 0x2d, 0x2d, 0xff},
+	color.RGBA{0x31, 0xd6, 0x89, 0xff},
+	color.RGBA{0x32, 0x32, 0x32, 0xff},
+	color.RGBA{0xff, 0xa8, 0xd9, 0xff},
+	color.RGBA{0xea, 0x9c, 0xc8, 0xff},
+	color.RGBA{0xd6, 0x90, 0xb7, 0xff},
+	color.RGBA{0xc1, 0x84, 0xa6, 0xff},
+	color.RGBA{0xad, 0x78, 0x96, 0xff},
+	color.RGBA{0x98, 0x6d, 0x85, 0xff},
+	color.RGBA{0x84, 0x61, 0x74, 0xff},
+	color.RGBA{0x6f, 0x55, 0x64, 0xff},
+	color.RGBA{0x5b, 0x49, 0x53, 0xff},
+	color.RGBA{0x46, 0x3d, 0x42, 0xff},
+}
+var locSize = 7
+
+type oldGroup struct {
+	frame int
+	locs  []diskLocation
+}
+
 func renderDefrag(events <-chan diskEvent) {
 	gridSize := 128
-	locSize := 7
 	var w, h int = locSize * 128, locSize * 128
-
-	var palette = []color.Color{
-		color.RGBA{0x2d, 0x2d, 0x2d, 0xff},
-		color.RGBA{0x31, 0xd6, 0x89, 0xff},
-		color.RGBA{0x32, 0x32, 0x32, 0xff},
-		color.RGBA{0xff, 0xa8, 0xd9, 0xff},
-	}
 
 	var images []*image.Paletted
 	var delays []int
-
+	oldGroups := []oldGroup{}
+	frame := 0
 	for e := range events {
-		if e.kind == done || e.kind == groupComplete {
+		if e.kind == groupComplete {
+			oldGroups = append(oldGroups, oldGroup{frame, e.disk.group})
+			continue
+		} else if e.kind == groupCleared {
 			continue
 		}
+
+		frame++
 
 		img := image.NewPaletted(image.Rect(0, 0, w, h), palette)
 		images = append(images, img)
@@ -36,28 +56,30 @@ func renderDefrag(events <-chan diskEvent) {
 			for y := 0; y < gridSize; y++ {
 				offsetX := x * locSize
 				offsetY := y * locSize
-				for i := 0; i < locSize; i++ {
-					img.Set(offsetX+i, offsetY, palette[0])
-					img.Set(offsetX+i, offsetY+locSize, palette[0])
-				}
-				for i := 0; i < locSize-2; i++ {
-					img.Set(offsetX, offsetY, palette[0])
-					img.Set(offsetX+locSize, offsetY+i+1, palette[0])
-				}
+
 				var c color.Color
 				if e.disk.grid[x][y] {
 					c = palette[1]
 				} else {
 					c = palette[2]
 				}
-				drawCell(img, c, offsetX, offsetY, locSize)
+
+				drawCell(img, c, offsetX, offsetY)
 			}
 		}
 
 		for _, l := range e.disk.group {
 			offsetX := l.x * locSize
 			offsetY := l.y * locSize
-			drawCell(img, palette[3], offsetX, offsetY, locSize)
+			drawCell(img, palette[3], offsetX, offsetY)
+		}
+
+		for _, g := range oldGroups {
+			for _, l := range g.locs {
+				offsetX := l.x * locSize
+				offsetY := l.y * locSize
+				drawCell(img, getFadeColor(frame-g.frame), offsetX, offsetY)
+			}
 		}
 	}
 
@@ -71,10 +93,55 @@ func renderDefrag(events <-chan diskEvent) {
 
 }
 
-func drawCell(img *image.Paletted, c color.Color, offsetX, offsetY, locSize int) {
-	for cellX := 0; cellX < locSize-2; cellX++ {
-		for cellY := 0; cellY < locSize-2; cellY++ {
-			img.Set(offsetX+cellX+1, offsetY+cellY+1, c)
+func getFadeColor(age int) color.Color {
+	switch {
+	case age < 5:
+		return palette[4]
+	case age < 10:
+		return palette[5]
+	case age < 15:
+		return palette[6]
+	case age < 20:
+		return palette[7]
+	case age < 30:
+		return palette[8]
+	case age < 40:
+		return palette[9]
+	case age < 60:
+		return palette[10]
+	case age < 80:
+		return palette[11]
+	default:
+		return palette[12]
+	}
+}
+
+func drawCell(img *image.Paletted, cell color.Color, offsetX, offsetY int) {
+	for x := 0; x < locSize; x++ {
+		for y := 0; y < locSize; y++ {
+			if x == 0 || x == locSize-1 || y == 0 || y == locSize-1 {
+				// border
+				img.Set(offsetX+x, offsetY+y, palette[0])
+			} else {
+				// cell
+				img.Set(offsetX+x, offsetY+y, cell)
+			}
 		}
 	}
+}
+
+func getCellColor(event *diskEvent, x, y int) color.Color {
+	for _, l := range event.disk.group {
+		if l.x == x && l.y == y {
+			if event.kind == groupCleared {
+				return palette[4]
+			}
+			return palette[3]
+		}
+	}
+
+	if event.disk.grid[x][y] {
+		return palette[1]
+	}
+	return palette[2]
 }
